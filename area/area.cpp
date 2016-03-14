378a0504b20bd6a03b7f55836fe1dc41c0ffe99c
Robert M. Koch <threadmetal@gmail.com>
2016-03-14 14:06:15 -0600

    misc exercises

---
+++ area/area.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "Enter the number of inches on a side and press enter: " << flush;
+	int inches;
+	cin >> inches;
+	cout << "The area is: " << inches*inches << " square inches." << endl;
+}
